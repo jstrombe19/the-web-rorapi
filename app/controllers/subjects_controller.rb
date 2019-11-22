2023066bd6689a92f2b4ffe9d936a6932faa4914
jstrombe19 <jaredstromberg@gmail.com>
2019-11-22 16:05:05 -0700

    Added index route as well as the private find_all_subjects before action to the SubjectsController.

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,13 @@
 class SubjectsController < ApplicationController
+  before_action: :find_all_subjects, only: [:index]
+
+  def index 
+    render json: @subjects 
+  end 
+
+  private
+
+  def find_all_subjects
+    @subjects = Subject.all 
+  end
 end
